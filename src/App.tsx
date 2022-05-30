4ed48588b04b3c535585a0617aeb09d67bd7a42d
AndriyIvaniukh <andriyivaniukh@gmail.com>
2022-05-31 00:32:15 +0300

    add

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React, {FC} from 'react';
 
 const App: FC = () => {
-    return (
-        <div>
-          App
-        </div>
-    );
+  return (
+      <div>
+        App
+      </div>
+  );
 };
 
 export {App};
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-
 import {App} from './App';
 
 
@@ -13,4 +12,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
